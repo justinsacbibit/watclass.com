6597e89b8d40d725db43cb993ed5f13883857c92
Justin Sacbibit <jsacbibit@gmail.com>
2015-01-16 19:09:26 -0500

    Fixed RNG, moved to server side logic

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ var express = require('express'),
 
 var port = process.env.PORT || 8080;
 
-app.use('/', express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'))
+app.get('/', function(req, res) {
+  var classes = ['DEATH KNIGHT', 'DRUID', 'HUNTER', 'MAGE', 'MONK', 'PALADIN', 'PRIEST', 'ROGUE', 'SHAMAN', 'WARLOCK', 'WARRIOR'];
+  var random = Math.floor((Math.random() * 11));
+  console.log(random);
+  res.render('index', { title: classes[random] });
+});
 app.listen(port);
--- public/main.js
+++
@@ -1,7 +0,0 @@
-var main = function() {
-  var classes = ['DEATH KNIGHT', 'DRUID', 'HUNTER', 'MAGE', 'MONK', 'PALADIN', 'PRIEST', 'ROGUE', 'SHAMAN', 'WARLOCK', 'WARRIOR'];
-  var random = Math.floor((Math.random() * 11) + 1);
-  $('span').text(classes[random]);
-}
-
-$(document).ready(main)
