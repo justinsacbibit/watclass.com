24a9619f32dd9a1176974463965587f11f10cbfd
Justin Sacbibit <jsacbibit@gmail.com>
2015-02-01 20:05:48 -0500

    Force shaman

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', function(req, res) {
   var classes = ['DEATH KNIGHT', 'DRUID', 'HUNTER', 'MAGE', 'MONK', 'PALADIN', 'PRIEST', 'ROGUE', 'SHAMAN', 'WARLOCK', 'WARRIOR'];
   var random = Math.floor((Math.random() * 11));
+  // force
+  random = 8;
   console.log(random);
   res.render('index', { title: classes[random] });
 });
